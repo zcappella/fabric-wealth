e0efbd7cbe5ea0a766e16e248a8e2716d95783d7
zcappella <zjcappella@gmail.com>
2017-07-07 08:49:51 -0400

    Initial model mock-up.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Size(models.Model):
+	size = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.size
+
+
+class Color(models.Model):
+	color = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.color
+
+
+class Widget(models.Model):
+	title = models.CharField(max_length=200)
+	size = models.ForeignKey(Size, on_delete=models.CASCADE)
+	color = models.ForeignKey(Color, on_delete=models.CASCADE)
+	inventory_number = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.title
+
+
+class Order(models.Model):
+	widgets = models.ManyToManyField(Widget)
+
+	def __str__(self):
+		return self.id
+
+	class Meta:
+		ordering = ('id',)
