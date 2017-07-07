357be0fda46dbd36abdead548213397a511df267
zcappella <zjcappella@gmail.com>
2017-07-07 09:05:57 -0400

    Registered admin sites

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from core.models import Widget, Color, Size, Order
+
 # Register your models here.
+
+admin.site.register(Widget)
+admin.site.register(Color)
+admin.site.register(Size)
+admin.site.register(Order)
